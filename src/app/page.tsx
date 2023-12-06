bde48a97276b215af46e251375f48a2e5b4f2744
Daniil Astakhov <daniil.astakhov.it@yandex.ru>
2023-12-06 16:41:46 +0300

    0.2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 
 export default function Home(): JSX.Element {
-	redirect("/login");
-	// return <div>v.0.7.1</div>;l;
+	// redirect("/login");
+	return <div>v.0.7.1</div>;
 }
